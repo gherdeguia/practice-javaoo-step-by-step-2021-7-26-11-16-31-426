3c8250589a1f8d4d2919323cde115a5e396710b1
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-04 16:43:25 +0800

    [JavaOOPExercise][Gerarld] feat: should_teacher_introduce_itself_with_which_class_it_teaches

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,8 +1,30 @@
 package practice05;
 
 public class Teacher extends Person {
+    private Integer klass;
 
     public Teacher(String name, int age) {
         super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+
+    @Override
+    public String introduce(){
+        //return "I am a Student. I am at Class "+this.klass+".";
+        //return "My name is "+super.name+". I am "+super.age+" years old. I am a Student. I am at Class "+this.klass+".";
+        if(this.klass != null){
+            return "My name is "+super.name+". I am "+super.age+" years old. I am a Teacher. I teach Class "+this.klass+".";
+        }else{
+            return "My name is "+super.name+". I am "+super.age+" years old. I teach No Class.";
+        }
+
+        //>My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.
+        //>My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+
     }
 }
